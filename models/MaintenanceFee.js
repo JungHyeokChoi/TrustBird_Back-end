11acabc29c4a307a4ff419bd9f50211ba8433ece
park <park129693@gmail.com>
2020-09-25 18:37:11 +0900

    add models

--- models/MaintenanceFee.js
+++ models/MaintenanceFee.js
@@ -2,6 +2,50 @@ var mongoose = require('mongoose')
 
 //Maintenance Fee Data
 var maintenanceFeeSchema = new mongoose.Schema({
+    claimingAgency : {
+        type : String,
+        required : true
+    },
+
+    electronicPaymentNum : {
+        type : Number,
+        required : true
+    },
+
+    dueDate : {
+        type : Date,
+        required : true
+    },
+
+    deadline : {
+        type : Date,
+        required : true
+    },
+
+    amountDue : {
+        type : Number,
+        required : true
+    },
+
+    amountDeadline : {
+        type : Number,
+        required : true
+    },
+
+    payment : {
+        type : String,
+        required : true
+    },
+
+    payer : {
+        type : String,
+        required : true
+    },
+
+    giro : {
+        type : String,
+        required : false
+    }
 
 })
 
--- models/User.js
+++ models/User.js
@@ -1,8 +1,65 @@
 var mongoose = require('mongoose')
+var bcrypt = require('bcryptjs')
 
 //User Data
-var userSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true 
+    },
 
+    email : {
+        type : String,
+        required : true
+    },
+
+    password : {
+        type : String,
+        required : true
+    },
+
+    dateOfBirth : {
+        type : Date,
+        required : true,
+    },
+
+    gender : {
+        type : String,
+        required : true
+    },
+
+    telephoneNum : {
+        type : String,
+        required : true
+    }
 })
 
-module.exports = mongoose.model('User', userSchema)
+UserSchema.pre('save', function(next){
+    const document = this
+
+    if(this.isNew || this.isModified('password')){
+        bcrypt.hash(this.password, 10, function(err, hashedPassword){
+            if(err){
+                next(err)
+            } 
+            else {
+                document.password = hashedPassword}
+                next()
+            }
+        )
+    } else {
+        next()
+    }
+})
+
+UserSchema.methods.isCorrectPassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, same){
+        if(err) {
+            callback(err)
+        } else {
+            callback(err, same)
+        } 
+    })
+}
+
+module.exports = mongoose.model('User', UserSchema)
