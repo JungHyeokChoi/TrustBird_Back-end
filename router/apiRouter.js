b98e9f4413b3c1f2886062e7267ca8ee13ae80d4
park young jun <park129693@gmail.com>
2020-09-28 16:22:08 +0900

    add router

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,8 +1,34 @@
+var User = require('../models/User')
+const crypto = require('crypto')
 var router = require('express').Router()
 
-router.route('/')
-    .get((req, res, next) => {
-        res.send("Success")
+//Router 분활
+const trustRouter = require('./trustRouter')
+
+const signRouter = require('./signRouter')
+
+const contractRouter = require('./contractRouter')
+
+const mainfeeRouter = require('./maintenanceFeeRouter')
+
+router.use('/trustapi', trustRouter)
+router.use('/signapi', signRouter)
+router.use('/contractapi', contractRouter)
+router.use('/mainfeeapi', mainfeeRouter)
+
+// hashing 함수
+router.route('/api/hash')
+    .post((req, res, next)=>{
+        const { username, password, email, dateOfBirth, gender, telephoneNum } = req.body
+
+        User.findOne({ username, password, email, dateOfBirth, gender, telephoneNum }, ()=>{
+
+            var userDate = (username + password + email + dateOfBirth + gender + telephoneNum)
+            console.log(userDate)
+            var crytoDB = crypto.createHash('sha256').update(userDate).digest('hex')
+            console.log(crytoDB)
+            res.sendStatus(200)
     })
+})
 
 module.exports = router;
