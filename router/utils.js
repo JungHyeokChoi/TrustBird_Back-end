355054c7ec39afa862ed538ebd5644b1d347caad
JungHyeokChoi <page1114@gmail.com>
2020-10-12 16:04:41 +0900

    Create Utils

---
+++ router/utils.js
@@ -0,0 +1,22 @@
+const crypto = require('crypto')
+const multer = require('multer')
+
+const jsonToHash = (data) => {
+    var hashData = "0x" + crypto.createHash('sha256').update(JSON.stringify(data)).digest('hex')
+
+    return hashData
+}
+
+const storage = multer.diskStorage({
+    destination : "./pubilc/img",
+    filename : function(req, file, cb) {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage, limits : { files : 10 }})
+
+module.exports = {
+    jsonToHash,
+    upload
+};
