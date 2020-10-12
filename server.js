b0ab4b2dd80efcb4f7bc88f42282f0107fcee4f4
JungHyeokChoi <page1114@gmail.com>
2020-10-12 16:06:44 +0900

    Refactoring

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
-var passport = require('passport')
-var mongoose = require('mongoose')
-var cors = require('cors')
-var session = require('express-session')
-var apiRouter = require('./router/apiRouter')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const session = require('express-session')
+
+const apiRouter = require('./router/apiRouter')
+
+const app = express()
 
 require('dotenv').config()
-
-var app = express()
+require('./router/passport/config').config(passport)
 
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(session({
     secret : "To Fill",
-    resave : true,
-    saveUninitialized : true
+    resave : false,
+    saveUninitialized : false,
+    cookie : {
+        httpOnly : true,
+        secure : false
+    }
 }))
 app.use(cors())
+app.use(express.static('pubilc'))
 
 //Connection Mongo DB
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
@@ -30,27 +37,6 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 //Passport
 app.use(passport.initialize())
 app.use(passport.session())
-passport.serializeUser((user, done) => {
-    done(null, user._id)
-})
-
-passport.deserializeUser((userId, done) => {
-    User.findById(userId, (err, result) => { done(err, result) })
-})
-
-const LocalStrategy = require('passport-local').Strategy
-const local = new LocalStrategy((username, password, done) => {
-    User.findOne({ username })
-    .then(user => {
-        if(!user || !user.validPassword(password)){
-            done(null, false, {message : "Invaild username password"})
-        } else {
-            done(null, user) 
-        }
-    })
-    .catch(e => done(e))
-})
-passport.use("local", local)
 
 //Add Routing File List on Middleware
 app.use('/', apiRouter)
