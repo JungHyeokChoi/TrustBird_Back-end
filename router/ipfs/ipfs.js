dcdca90269956640596c803d0785e1338695e766
JungHyeokChoi <page1114@gmail.com>
2020-10-25 22:29:29 +0900

    Create IPFS folder

---
+++ router/ipfs/ipfs.js
@@ -0,0 +1,38 @@
+const ipfsAPI = require('ipfs-api')
+const fs =require('fs')
+
+const ipfs = ipfsAPI({
+	host: '127.0.0.1',
+	port: 5001,
+	protocol: 'http'
+});
+
+const ipfs_method = {
+    add : async(file) => {
+        const readData = await fs.readFileSync(file.path)
+
+        const data = {
+            path : `${file.savePath}/${file.name}`,
+            content : readData
+        }   
+    
+        const result = await ipfs.add(data)
+
+        await fs.unlink(file.path, (err) => {
+            if (err) return console.log(err)
+        })	
+
+        return {
+            filePath : result[0].hash,
+            fileName : file.name
+        }
+    },
+    
+    get : async(file) => {
+        const chunk = await ipfs.get(file.cid)
+
+        await fs.writeFileSync(file.savePath, chunk[0].content)
+    }
+}
+
+module.exports = ipfs_method
